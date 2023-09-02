e5005be48b29211c33f09489dc8ddc11f4277dcf
augustorcn <augusto.r.c.napoleao@gmail.com>
2023-09-02 20:54:09 -0300

    feat: add SendEmailWhenProductIsCreated

--- src/infra/product/eventHandlers/SendEmailWhenProductIsCreated.ts
+++ src/infra/product/eventHandlers/SendEmailWhenProductIsCreated.ts
@@ -1,7 +1,7 @@
 import EventHandler from "../../../domain/@shared/events/EventHandler";
 import ProductCreated from "../../../domain/product/events/ProductCreated";
 
-export default class SendEmailWhenProductIsCreatedHandler
+export default class SendEmailWhenProductIsCreated
 	implements EventHandler<ProductCreated>
 {
 	handle(event: ProductCreated): void {
