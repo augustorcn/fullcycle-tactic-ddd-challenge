1745a762c978c521e9b03e409029146bc09f4f3b
augustorcn <augusto.r.c.napoleao@gmail.com>
2023-09-02 20:48:47 -0300

    feat: add ProductCreated domain event

---
+++ src/domain/product/events/ProductCreated.ts
@@ -0,0 +1,11 @@
+import Event from "../../@shared/events/Event";
+
+export default class ProductCreated implements Event {
+	dataTimeOccurred: Date;
+	eventData: any;
+
+	constructor(eventData: any) {
+		this.dataTimeOccurred = new Date();
+		this.eventData = eventData;
+	}
+}
