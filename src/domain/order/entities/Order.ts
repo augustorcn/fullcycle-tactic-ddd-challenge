213056439277babd443734264a5013a4ae7c07db
augustorcn <augusto.r.c.napoleao@gmail.com>
2023-09-01 23:53:55 -0300

    refactor: refactor order aggregate

--- src/domain/order/entities/Order.ts
+++ src/domain/order/entities/Order.ts
@@ -1,25 +1,39 @@
 import OrderItem from "./OrderItem";
 
 export default class Order {
-	constructor(
-		readonly id: string,
-		readonly customerId: string,
-		readonly items: OrderItem[]
-	) {
-		this.id = id;
-		this.customerId = customerId;
-		this.items = items;
+	private _id: string;
+	private _customerId: string;
+	private _items: OrderItem[];
+	private _total: number;
+
+	constructor(id: string, customerId: string, items: OrderItem[]) {
+		this._id = id;
+		this._customerId = customerId;
+		this._items = items;
+		this._total = this.getTotal();
 		this.validate();
 	}
 
+	get id(): string {
+		return this._id;
+	}
+
+	get customerId(): string {
+		return this._customerId;
+	}
+
+	get items(): OrderItem[] {
+		return this._items;
+	}
+
 	validate(): boolean {
-		if (this.id.length === 0) {
+		if (this._id.length === 0) {
 			throw new Error("Id is required");
 		}
-		if (this.customerId.length === 0) {
+		if (this._customerId.length === 0) {
 			throw new Error("CustomerId is required");
 		}
-		if (this.items.length === 0) {
+		if (this._items.length === 0) {
 			throw new Error("Items are required");
 		}
 
@@ -27,7 +41,7 @@ export default class Order {
 	}
 
 	getTotal(): number {
-		return this.items.reduce(
+		return this._items.reduce(
 			(acc, item) => acc + item.getTotal(),
 			0
 		);
