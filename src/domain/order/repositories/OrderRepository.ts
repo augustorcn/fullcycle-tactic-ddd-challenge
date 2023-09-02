7ecffffc2e6b101f03eb487f45fdaf15c0133888
augustorcn <augusto.r.c.napoleao@gmail.com>
2023-09-02 00:12:42 -0300

    feat: add OrderRepository

---
+++ src/domain/order/repositories/OrderRepository.ts
@@ -0,0 +1,4 @@
+import BaseRepository from "../../@shared/repositories/BaseRepository";
+import Order from "../entities/Order";
+
+export default interface OrderRepository extends BaseRepository<Order> {}
