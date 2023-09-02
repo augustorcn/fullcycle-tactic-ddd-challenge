323bbd2bdfe2afa3ff6e22e5027b1bbb50143d3a
augustorcn <augusto.r.c.napoleao@gmail.com>
2023-09-02 00:11:48 -0300

    feat: add ProductRepository

---
+++ src/domain/product/repositories/ProductRepository.ts
@@ -0,0 +1,4 @@
+import BaseRepository from "../../@shared/repositories/BaseRepository";
+import Product from "../entities/Product";
+
+export default interface ProductRepository extends BaseRepository<Product> {}
