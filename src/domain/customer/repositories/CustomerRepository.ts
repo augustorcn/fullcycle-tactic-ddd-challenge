9e0442571763623671fc445627764750aab4e5a8
augustorcn <augusto.r.c.napoleao@gmail.com>
2023-09-02 00:13:37 -0300

    feat: add CustomerRepository

---
+++ src/domain/customer/repositories/CustomerRepository.ts
@@ -0,0 +1,4 @@
+import BaseRepository from "../../@shared/repositories/BaseRepository";
+import Customer from "../entities/Customer";
+
+export default interface CustomerRepository extends BaseRepository<Customer> {}
